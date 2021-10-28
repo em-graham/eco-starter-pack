03eff678b289d0256813454671392db5b366dec5
Emilee Graham <emgraham@Emilees-MacBook-Air-2.local>
2021-10-28 20:22:55 +0100

    Initial commit

---
+++ src/main/java/org/launchcode/ecostarterpack/EcoStarterPackApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.ecostarterpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoStarterPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoStarterPackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/ecostarterpack/EcoStarterPackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.ecostarterpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoStarterPackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
